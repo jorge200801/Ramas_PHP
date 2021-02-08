6163fb6ad28356d2af937bbba7258993a893510a
jorge <jorge@hotmail.com>
2021-02-08 09:47:03 +0100

    Rama principal

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title></title>
+<meta charset="UTF-8">
+</head>
+<body>
+<form name="formulario" action="procesar.php" method="POST">
+	Introduce tu nombre: <input type="text" name="nombre">
+	Introduce la edad: <input type="text" name="edad">
+	<input type="submit" value="Enviar">
+</form>
+</body>
+</html>
